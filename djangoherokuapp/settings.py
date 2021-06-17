bfbdcfbeb9b04152bdef8322ef6d7d8b2cc1d23b
Ted Teah <from@dabronx.nyc>
2021-06-17 17:55:27 -0400

    5

--- djangoherokuapp/settings.py
+++ djangoherokuapp/settings.py
@@ -117,10 +117,12 @@ USE_L10N = True
 
 USE_TZ = True
 
-
+CORS_ORIGIN_ALLOW_ALL = True 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000',
+] 
 STATIC_URL = '/static/'
 django_heroku.settings(locals())
 
