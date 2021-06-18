00fba239cd33bf6894b1160852d8f22ddf3ab65e
Ted Teah <from@dabronx.nyc>
2021-06-18 13:54:18 -0400

    map loaded and displaying data

--- apiapp/models.py
+++ apiapp/models.py
@@ -8,6 +8,9 @@ class Event(models.Model):
     summary = models.TextField(max_length=1000, help_text='What is what')
     url = models.CharField(max_length=300, help_text='To Store Img', null=True, default="Aspirational",)
     user = models.ForeignKey(User, on_delete=models.DO_NOTHING, null=True)
-    
+    lat = models.FloatField(default=0)
+    latRef = models.CharField(max_length=1, default='X')
+    long = models.FloatField(default=0) 
+    longRef = models.CharField(max_length=1, default='X')
     def __str__(self):
         return self.title
