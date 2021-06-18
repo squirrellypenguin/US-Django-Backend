527191f778d8c2a3b52173d85f2eeabffa6cc122
Ted Teah <from@dabronx.nyc>
2021-06-18 06:56:14 -0400

    working admin

--- apiapp/serializers.py
+++ apiapp/serializers.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User, Group
 class EventSerializer(serializers.ModelSerializer):
     class Meta:
         model = Event
-        fields = [ "id", "title", "summary", "url"]
+        fields = [ "id", "title", "summary", "url", 'user']
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
