8bf5639e59f29f684e20df3064001cefee78c9e8
Ted Teah <from@dabronx.nyc>
2021-06-18 06:37:02 -0400

    working admin

--- apiapp/admin.py
+++ apiapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Event
+
+class EventAdmin(admin.ModelAdmin):
+
+    list_display = ("title", 'summary', 'user')
+
+admin.site.register(Event, EventAdmin)
--- apiapp/models.py
+++ apiapp/models.py
@@ -1,8 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 class Event(models.Model):
     title = models.CharField(max_length=300)
     summary = models.TextField(max_length=1000, help_text='What is what')
     url = models.CharField(max_length=300, help_text='To Store Img', null=True, default="Aspirational",)
-
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING, null=True)
+    
+    def __str__(self):
+        return self.title
